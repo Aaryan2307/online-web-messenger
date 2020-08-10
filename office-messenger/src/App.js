6cbb183639b34bc6697137b94bc31233a52c3044
Aaryan2307 <aaryan23@live.com>
2020-08-10 14:33:28 +0100

    added some boilerplate and initial login format

--- office-messenger/src/App.js
+++ office-messenger/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import Login from './components/Login'
+import store from './store/store'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
+    <Provider store={store}>
     <div className="App">
-      Hi this is my app!
+      This is my react app
+      <Login />
     </div>
+    </Provider>
   );
 }
 
